5f3843054715173105ef0f7168ebf430275db42f
betussozgen <fatmabetulozgen@hotmail.com>
2023-06-13 16:02:35 +0300

    app page

--- src/App.tsx
+++ src/App.tsx
@@ -14,65 +14,30 @@ import {
 } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import database from '@react-native-firebase/database';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './pages/auth/Login/Login';
+import Sign from './pages/auth/Sign/Sign';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
+  const AuthStack = () => {
+    return (
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='LoginPage' component={Login} />
+        <Stack.Screen name='SignPage' component={Sign} />
+      </Stack.Navigator>
 
-  const checkDB = () => {
-
-    const reference = database().ref('books/');
-    reference.once('value')
-      .then(snapshot => {
-        const response = snapshot.val()
-        console.log(response);
-      });
+    )
   }
-
-  const listenDB = () => {
-    const reference = database().ref('books/1');
-    reference.on('value', snapshot => {
-      console.log(snapshot.val());
-    });
-
-  }
-
-  const setDB = () => {
-    const reference = database().ref('car/rentable');
-    reference.set({
-      brand: 'Audi',
-      model: 'A8',
-      price: 128,
-    })
-  }
-
-
-  const updateDB = () => {
-    const reference = database().ref('car/rentable');
-    reference.update({
-      model: 'A3',
-
-    })
-
-  }
-
-
-  const pushDB = () => {
-    const reference = database().ref('car/rentable');
-    reference.push({
-      brand: 'Passat',
-      model: '98',
-      price: 111,
-    })
-  }
-
   return (
-    <SafeAreaView>
-      <Text>Hello Firebase</Text>
-      <Button title="Check DB" onPress={checkDB} />
-      <Button title="Listen DB" onPress={listenDB} />
-      <Button title="Set DB" onPress={setDB} />
-      <Button title="Update DB" onPress={updateDB} />
-      <Button title="Push DB" onPress={pushDB} />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='AuthStack' component={AuthStack} />
+        {/* <Stack.Screen name='SignPage' component={Sign} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
