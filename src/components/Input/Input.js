05b61e3c65ca0124132e35fbccce5c5933600786
betussozgen <fatmabetulozgen@hotmail.com>
2023-06-13 16:01:55 +0300

    ınput component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+import styles from "./Input.style";
+
+
+const Input = ({ placeholder, value, onType }) => {
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                placeholder={placeholder}
+                onChangeText={onType}
+                value={value}
+            />
+
+        </View>
+    )
+}
+
+export default Input;
